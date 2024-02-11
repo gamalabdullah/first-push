e46ea03a20e21253858366d872630178c57552b7
gamalabdullahnasser <gamalabdullahnasser@gmail.com>
2024-02-11 21:08:31 +0800

    add contact form check

--- js/main.js
+++ js/main.js
@@ -10,3 +10,9 @@ let portfolioGallery = document.querySelector('.portfolio-gallery');
 if (portfolioGallery) {
   console.log('Welcome to the Portfolio page');
 }
+
+let contactForm = document.querySelector('.contact-form');
+
+if (contactForm) {
+  console.log('Welcome to the Contact page');
+}
